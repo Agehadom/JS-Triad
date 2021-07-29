775898722c75a422f5e4e853ae011e903d967160
Agehadom <upwithice@yahoo.com>
2021-07-28 19:59:07 -0600

    ThreeJS demo page container ready for rendering

--- Triad/src/app/app.module.ts
+++ Triad/src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ThreeComponent } from './three/three.component';
-import { ThreeCoreModule } from '@angular-three/core';
-import { ThreeBoxBufferGeometryModule } from '@angular-three/core/geometries';
-import { ThreeMeshBasicMaterialModule } from '@angular-three/core/materials';
-import { ThreeMeshModule } from '@angular-three/core/meshes';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -18,16 +14,11 @@ import { RouterModule, Routes } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    ThreeCoreModule,
-    ThreeMeshModule,
-    ThreeBoxBufferGeometryModule,
-    ThreeMeshBasicMaterialModule,
-
     RouterModule.forRoot([
       {path: 'cummies', component: MainComponent},
       {path: 'threetest', component: ThreeComponent},
     ]),
-    
+
     AppRoutingModule
   ],
   providers: [],
--- Triad/src/app/three/three.component.ts
+++ Triad/src/app/three/three.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import * as THREE from 'three';
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
 
-const scene = new THREE.Scene();
+const container = document.querySelectorAll('#scene-container');
 
 @Component({
   selector: 'app-three',
@@ -9,10 +18,52 @@ const scene = new THREE.Scene();
   styleUrls: ['./three.component.css']
 })
 export class ThreeComponent implements OnInit {
+  name = 'AngThree';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    console.log("It's all Init from here!");
+    
+
   }
 
+  ngAfterViewInit() {
+
+    console.log("This is After the view is initiated.");
+
+    const scene = new Scene();
+    scene.background = new Color('red')
+
+    const length = 2
+    const width = 2
+    const depth = 2
+    const geo = new BoxBufferGeometry(length,width,depth)
+    const mat = new MeshBasicMaterial()
+    const shape = new Mesh(geo, mat)
+
+    scene.add(shape)
+
+    const fov = 90
+    const aspect = 4 / 3
+    const near = 0.1
+    const far = 100
+
+    const camera = new PerspectiveCamera(fov, aspect, near, far)
+    camera.position.set(0, 0, 10)
+
+    const renderer = new WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // container.append(renderer.domElement);
+  }
+
+  Getcheck () {
+    if (container == null) {
+      console.log("Yeah, there's nothing inside.")
+    } 
+    else console.log("Container found!");
+    };
+
 }
+
