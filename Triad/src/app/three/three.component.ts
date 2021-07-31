ec283bcf1c91ad1104cb8e946b2a85001066b046
Agehadom <upwithice@yahoo.com>
2021-07-30 19:24:56 -0600

    Three component now responsing to changes
    Things can now be properly injected into the scenecontainer in the three demo page.

--- Triad/src/app/three/three.component.ts
+++ Triad/src/app/three/three.component.ts
@@ -1,40 +1,54 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, Inject, Directive, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import * as THREE from 'three';
-import {
-  BoxBufferGeometry,
-  Color,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from 'three';
+import { BoxBufferGeometry, Color, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
-const container = document.querySelectorAll('#scene-container');
+// const container = document.querySelectorAll('#scenecontainer');
 
-@Component({
+@Directive ({
+  selector: '.scenecontainer'
+  })
+
+  export class Container {
+  @Input() id!: string;
+}
+
+@Component ({
   selector: 'app-three',
   templateUrl: './three.component.html',
   styleUrls: ['./three.component.css']
 })
-export class ThreeComponent implements OnInit {
-  name = 'AngThree';
 
-  constructor() { }
+export class ThreeComponent implements OnInit, AfterViewInit {
 
-  ngOnInit() {
-
-    console.log("It's all Init from here!");
-    
+  constructor(private elementRef:ElementRef) {
 
   }
 
+  // @ViewChild('Container') container: ElementRef;
+  // name = 'AngThree';
+
+  // constructor (@Inject(DOCUMENT) document: Document) {
+  //   const container = document.getElementById('#scenecontainer')
+  // }
+
+  ngOnInit() {
+
+    const container = this.elementRef.nativeElement.querySelector('.scenecontainer')
+
+    container.insertAdjacentHTML('beforeend', '<div>It finally fucking works.</div>')
+    
+  }
+
   ngAfterViewInit() {
+  
+    const container = this.elementRef.nativeElement.querySelector('.scenecontainer')
 
     console.log("This is After the view is initiated.");
-
+    console.log(this);
+    
     const scene = new Scene();
-    scene.background = new Color('red')
+    scene.background = new Color('blue')
 
     const length = 2
     const width = 2
@@ -46,7 +60,7 @@ export class ThreeComponent implements OnInit {
     scene.add(shape)
 
     const fov = 90
-    const aspect = 4 / 3
+    const aspect = container.clientWidth / container.clientHeight
     const near = 0.1
     const far = 100
 
@@ -55,15 +69,19 @@ export class ThreeComponent implements OnInit {
 
     const renderer = new WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // container.append(renderer.domElement);
+
   }
 
   Getcheck () {
-    if (container == null) {
-      console.log("Yeah, there's nothing inside.")
-    } 
-    else console.log("Container found!");
-    };
+    if (Container) {
+      console.log('Button pressed!');
+      console.log(this);
 
+      if (this.elementRef.nativeElement.children[0].children[1].innerHTML = 'It finally fucking works.') {
+        this.elementRef.nativeElement.children[0].children[1].innerHTML = 'Yep, you got it.'
+      } else
+      this.elementRef.nativeElement.children[0].children[1].innerHTML = 'Huh?'
+    } 
+  };
 }
 
